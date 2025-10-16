48ff11f916cf0e910eb77a0e90bd58710c639421
SalmaWaleeddd <salmawaleed2030@outlook.com>
2025-10-16 13:50:16 +0300

    add task-manager with state management and localStorage persistence

--- src/app/task-manager/task-manager.component.ts
+++ src/app/task-manager/task-manager.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { TaskListComponent } from '../task-list/task-list.component';
+import { TaskInputComponent } from '../task-input/task-input.component';
+import { Task } from '../models/task';
 
 @Component({
   selector: 'app-task-manager',
-  imports: [],
+  imports: [TaskInputComponent, TaskListComponent],
   templateUrl: './task-manager.component.html',
   styleUrl: './task-manager.component.scss'
 })
 export class TaskManagerComponent {
+  tasks: Task[] = [];
 
+
+  ngOnInit() {
+    const savedTasks = localStorage.getItem('tasks');
+    if (savedTasks) {
+      this.tasks = JSON.parse(savedTasks);
+    }
+  }
+
+  addTask(task: Task) {
+    this.tasks.push(task);
+    this.saveToLocalStorage();
+  }
+
+  deleteTask(task: Task) {
+    this.tasks = this.tasks.filter(t => t.id !== task.id);
+    this.saveToLocalStorage();
+  }
+
+  toggleTask(task: Task) {
+    const t = this.tasks.find(t => t.id === task.id);
+    if (t) {
+      t.completed = !t.completed;
+      this.saveToLocalStorage();
+    }
+  }
+
+  saveToLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
 }
