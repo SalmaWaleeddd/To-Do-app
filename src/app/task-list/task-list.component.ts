221d5cf8ee5f774a106394879fb85a46d6439c1d
SalmaWaleeddd <salmawaleed2030@outlook.com>
2025-10-16 12:46:25 +0300

    implement task list with complete/delete functionality

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component , output, input} from '@angular/core';
+import { NgClass } from '@angular/common';
+
+import { Task } from '../models/task';
+
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
 })
 export class TaskListComponent {
 
+  tasks=input<Task[]>();
+  deleteTask= output<Task>();
+  toggleTask= output<Task>();
+
+  toggleTaskCompletion(task: Task) {
+    this.toggleTask.emit(task);
+    console.log("Toggled Task:", task);
+  }
+
+  onDeleteTask(task: Task) {
+    this.deleteTask.emit(task);
+    console.log("Deleted Task:", task);
+
+  }
 }
